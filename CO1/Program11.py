8b893de37ff04212d8fbdfbd5a89b5deb7542a81
Rahul628226 <93213868+Rahul628226@users.noreply.github.com>
2022-01-09 14:41:57 +0530

    Update Program11.py

--- CO1/Program11.py
+++ CO1/Program11.py
@@ -1,12 +1,14 @@
 #  Find biggest of 3 numbers entered.
 
-print("enter 3 numbers:")
-a = int(input())
-b = int(input())
-c = int(input())
-if a > b and a > c:
-    print("largest number is :", a)
-elif b > a and b > c:
-    print("largest number is :", b)
+a=int(input("Enter the number of first element"))
+b=int(input("Enter the second number"))
+c=int(input("Enter the third number"))
+if a>b:
+    if a>c:
+        print(a)
+    else:
+        print(c)
+elif b>c:
+    print(b)
 else:
-    print("largest number is :", c)
+    print(c)
